c0c42a6d390a4c857a049427c0ea67f9fddd0772
Harshal17S <harshal.ingle17@gmail.com>
2025-01-11 14:40:14 +0530

    CSS

--- frontend/src/components/CustomComponents/Login/Login.jsx
+++ frontend/src/components/CustomComponents/Login/Login.jsx
@@ -1,75 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { json, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-const Login=()=>{
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
-    const navigate = useNavigate();
+  const login = async () => {
+    if (localStorage.getItem("user")) {
+      const userObject = JSON.parse(localStorage.getItem("user"));
+      const userEmail = userObject.email;
+      const userPassword = userObject.password;
 
-   const login=async()=>{
-    console.warn(email,password)
-    // let result = await fetch("http://localhost:3000/login", {
-    //     method: "post",
-    //     body: JSON.stringify({ email, password }),
-    //     headers: {
-    //       "content-type": "application/json",
-    //     },
-    //   });
-    //   result = await result.json();
-     
-      if(localStorage.getItem("user")){
-       const userobject=JSON.parse(localStorage.getItem("user"));
-        const user_email=userobject.email;
-        const user_pass=userobject.password;
-        console.log(user_email+" "+email);
-
-        if(user_pass==password && user_email==email){
-          //  
-          // navigate("https://youtube-video-summary-bds6.onrender.com");
-          // <href>https://youtube-video-summary-bds6.onrender.com</href>
-window.open("https://youtube-video-summary-bds6.onrender.com");
-        }
-        else{
-          alert("Please Enter correct mail/password")
-        }
+      if (userPassword === password && userEmail === email) {
+        window.open("https://youtube-video-summary-bds6.onrender.com");
+      } else {
+        alert("Please enter correct email/password.");
       }
-     
-
     }
+  };
 
-    return(
-        <div className="register_form">
-         <h1 className="h1tag text-3xl ">Form</h1>
-      <div className="second">
-      <input
-          className="input_box"
-          type="text"
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e)=>setemail(e.target.value)}
-          
-        />
-        <input
-          className="input_box"
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e)=>setpassword(e.target.value)}
-        />
-        <button onClick={login} className="signupbutt  text-red-300">
+  return (
+    <div className="container">
+      <h1>Login Form</h1>
+      <form>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" className="loginButton" onClick={login}>
           Login
         </button>
         <button
-      onClick={() => navigate('/sign')}
-      className="signupbutt text-red-300"
-    >
-      No Account
-    </button>
-      </div>
-        </div>
-
-    );
+          type="button"
+          className="loginButton"
+          onClick={() => navigate("/sign")}
+        >
+          No Account
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/CustomComponents/Signup/Signup.jsx
+++ frontend/src/components/CustomComponents/Signup/Signup.jsx
@@ -1,62 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import "./Signup.css";
+
 const Signup = () => {
   const [name, setName] = useState("");
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
   useEffect(() => {
     const auth = localStorage.getItem("user");
     if (auth) {
       navigate("/");
     }
   }, []);
-  const CollectData = async () => {
-    console.warn(name, email, password);
-    // let result = await fetch("http://localhost:3000/sign", {
-    //   method: "post",
-    //   body: JSON.stringify({ name, email, password }),
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    // });
-    // result = await result.json();
-    // console.warn(result);
 
-    localStorage.setItem("user",JSON.stringify({ name, email, password }));
+  const collectData = async () => {
+    console.warn(name, email, password);
+    localStorage.setItem("user", JSON.stringify({ name, email, password }));
     navigate("/");
   };
+
   return (
-    <div className="register_form">
-      <h1 className="h1tag text-3xl ">Form</h1>
-      <div className="second">
-        <input
-          className="input_box"
-          type="text"
-          placeholder="Enter your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          className="input_box"
-          type="text"
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setemail(e.target.value)}
-        />
-        <input
-          className="input_box"
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setpassword(e.target.value)}
-        />
-        <button onClick={CollectData} className="signupbutt  text-red-300">
+    <div className="container">
+      <h1>Signup Form</h1>
+      <form>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="button"
+          className="loginButton"
+          onClick={collectData}
+        >
           Signup
         </button>
-      </div>
+      </form>
     </div>
   );
 };
